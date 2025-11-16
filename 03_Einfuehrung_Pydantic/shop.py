b7cfa90467561a7935252c9bd638a255a8ac0b68
Cappuccinomeister96 <Daniel.Reinhardt1996@gmx.de>
2025-11-16 20:29:00 +0100

    pydantic tests

---
+++ 03_Einfuehrung_Pydantic/shop.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    name: str = Field(min_length=3)
+    age: int = Field(ge=0)
+    email: str = Field(min_length=5)
+
+
+if __name__ == "__main__":
+ 
+    tests = [
+        {"name": "Alice", "age": 25, "email": "alice@example.com"},
+        {"name": "Al", "age": 25, "email": "alice@example.com"},
+        {"name": "Bob", "age": -5, "email": "bob@example.com"},
+        {"name": "Charlie", "age": 30, "email": "c@b"},
+    ]
+
+    for test in tests:
+        print("----------------------------\n")
+        try:
+            user = User(**test)
+            print(f"Erfolgreich erstellt: \n{user}")
+        except Exception as e:
+            print(f"Fehler bei der Erstellung von \n{test}: \n{e}")
+   
+
